f10e20ed133824036966b01423db97989f5c8b85
casey bo fritsch <bo@caseys-MacBook-Pro.local>
2023-05-12 12:23:12 -0700

    refactor triggerstampt

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let canvas = document.getElementById('canvas');
   let cntx = canvas.getContext("2d");
   let isPainting = false;
+  let isErasing = false;
 
 // set up variables to store current path and drawing history
   let path = [];
@@ -12,13 +13,25 @@ document.addEventListener("DOMContentLoaded", function() {
   let makesworse = new Image();
   makebetter.src = `images/makebetter.png`;
   makesworse.src = `images/makesworse.png`;
-  
 
 // initializing line style
   cntx.lineCap = 'round';
   cntx.lineJoin = "round";
   cntx.lineWidth = 15;
   cntx.strokeStyle = `rgb(255, 0, 0, 0.5)`;
+
+  // line style controls
+  let lineWidth = document.getElementById('lineWidth');
+  let intensity = document.getElementById('intensity');
+  lineWidth.addEventListener("input", (e) => {cntx.lineWidth = lineWidth.value;});
+  intensity.addEventListener("input", (e) => {cntx.strokeStyle = `rgb(255, 0, 0, ${intensity.value})`;});
+
+// draw background body outline
+  // let img = document.getElementById('bodyoutline');
+  // img.addEventListener('load', (e) => {
+  //   cntx.drawImage(img, 0, 0,canvas.width, canvas.height);
+  //   imgdata.push(cntx.getImageData(0,0,canvas.width, canvas.height));
+  // });
 
   // initialize trigger types object
   let triggers = {
@@ -29,49 +42,89 @@ document.addEventListener("DOMContentLoaded", function() {
     custom: {custom:'custom'}
   };
 
+  //set up triggers event listeners, radio options, and images
+  let triggerbutton = document.getElementById('trigger');
+  let triggertypebox = document.getElementById("triggertypes");
+
   for (let key in triggers) {
-    let currenttrigger = triggers[key];
-    currenttrigger.icon = new Image();
-    currenttrigger.icon.src = `images/${key}icon.png`;
-    currenttrigger.element = document.getElementById(key);
-    currenttrigger.radio = currenttrigger.element.nextElementSibling;
-    currenttrigger.radiobetter = document.getElementById(`${key}better`);
-    currenttrigger.radioworse = document.getElementById(`${key}worse`);
-    currenttrigger.radioworse.addEventListener('change', ()=>{
-      if (currenttrigger.radioworse.checked) {
-        console.log(key, 'better',currenttrigger.radiobetter.checked, 'worse',currenttrigger.radioworse.checked,);
+    triggers[key].icon = new Image();
+    triggers[key].icon.src = `images/${key}icon.png`;
+    triggers[key].element = document.getElementById(key);
+    triggers[key].radio = triggers[key].element.nextElementSibling;
+    triggers[key].radiobetter = document.getElementById(`${key}better`);
+    triggers[key].radioworse = document.getElementById(`${key}worse`);
+    triggers[key].radioworse.addEventListener('change', ()=>{
+      if (triggers[key].radioworse.checked) {
+        // console.log(key, 'better',triggers[key].radiobetter.checked, 'worse',triggers[key].radioworse.checked,);
       }
     });
-    currenttrigger.radiobetter.addEventListener('change', ()=>{
-      if (currenttrigger.radiobetter.checked){
-        console.log(key, 'better',currenttrigger.radiobetter.checked, 'worse',currenttrigger.radioworse.checked,);
+    triggers[key].radiobetter.addEventListener('change', ()=>{
+      if (triggers[key].radiobetter.checked){
+        // console.log(key, 'better',triggers[key].radiobetter.checked, 'worse',triggers[key].radioworse.checked,);
       } 
     });
-    currenttrigger.element.addEventListener('click', ()=>{
-      if ( currenttrigger.element.checked){
-        currenttrigger.radio.style.display = 'contents';
+    triggers[key].element.addEventListener('click', ()=>{
+      if ( triggers[key].element.checked){
+        triggers[key].radio.style.display = 'contents';
       } else {
-        currenttrigger.radio.style.display = 'none';
+        triggers[key].radio.style.display = 'none';
       };
     });
   };
 
-// toolbar listeners and functions
-  // line style controls
-  let lineWidth = document.getElementById('lineWidth');
-  let intensity = document.getElementById('intensity');
-  lineWidth.addEventListener("input", (e) => {cntx.lineWidth = lineWidth.value;});
-  intensity.addEventListener("input", (e) => {cntx.strokeStyle = `rgb(255, 0, 0, ${intensity.value})`;});
+  triggerbutton.addEventListener('click', (e) => {
+    canvas.removeEventListener("touchstart",onStart);
+    canvas.removeEventListener("mousedown",onStart);
+    
+    canvas.addEventListener('mousedown', drawtrigger);
+    canvas.addEventListener('touchstart', drawtrigger);
+    canvas.addEventListener("mouseup", triggerstamp);
+    canvas.addEventListener("touchend", triggerstamp);
 
+    triggertypebox.style.display = "flex"; //to make it not hidden
+    triggerbutton.style.display = "none";
+  });
 
-// draw background body outline
-  let img = document.getElementById('outlineimg');
-  img.addEventListener('load', (e) => {
-      cntx.drawImage(img, 0, 0,canvas.width, canvas.height);
-      imgdata.push(cntx.getImageData(0,0,canvas.width, canvas.height));
-    });
+  //close triggers
+  let hidetriggers = document.getElementById('closetrigger');
+  function closetriggers() {
+    triggertypebox.style = 'display: none';
+    triggerbutton.style.display = "initial";
+
+    canvas.removeEventListener("mouseup", triggerstamp);
+    canvas.removeEventListener("touchend", triggerstamp);
+    canvas.removeEventListener('mousedown', drawtrigger);
+    canvas.removeEventListener('touchstart', drawtrigger);
+    canvas.addEventListener("mousedown",onStart);
+    canvas.addEventListener("touchstart",onStart);
+
+    cntx.lineWidth = lineWidth.value;
+    cntx.strokeStyle = `rgb(255, 0, 0, ${intensity.value})`;
+
+    for (let key in triggers) {
+      triggers[key].radio.style.display = 'none';
+      triggers[key].element.checked = false;
+    }
+  }
+  hidetriggers.addEventListener('click',()=>{
+   closetriggers();
+  })
+
+  //show & hide instructions
+  let instructions = document.getElementById('showinstructiondisplay');
+  let instructionsbutton = document.getElementById('showinstructionsbutton');
+  instructionsbutton.addEventListener('click', ()=>{
+    instructions.style = 'display: contents';
+    instructionsbutton.style = 'display: none;'
+  });
+  let hidebutton = document.getElementById('hideinstructions');
+  hidebutton.addEventListener('click',()=>{
+    instructions.style = 'display: none';
+    instructionsbutton.style = 'display: initial;'
+  })
 
 // action controls
+// UNDO
   let undo = document.getElementById('undo');
   undo.addEventListener('click', (e) => {
     if (imgdata.length > 1) {
@@ -81,13 +134,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+// CLEAR
     let clearbutton = document.getElementById('clear');
     clearbutton.addEventListener("click", () => {
       imgdata = [imgdata[0]];
-      // cntx.putImageData(imgdata.at(-1),0,0);
-      cntx.drawImage(img, 0, 0,canvas.width, canvas.height);
+      cntx.putImageData(imgdata.at(-1),0,0);
     });
 
+//SAVE
   let savebutton = document.getElementById('savebutton');
   savebutton.addEventListener("click", () => {
     const link = document.createElement("a");
@@ -95,6 +149,13 @@ document.addEventListener("DOMContentLoaded", function() {
     link.href = canvas.toDataURL();
     link.click();
   });
+
+  //ERASE 
+  // let erasebutton = document.getElementById('erasebutton');
+  // erasebutton.addEventListener("click", ()=>{
+  //   cntx.strokeStyle = `rgb(255, 255, 255, 1)`;
+  //   isErasing = true;
+  // });
 
 // main drawing process listeners and functions
   canvas.addEventListener("mousedown",onStart);
@@ -104,11 +165,19 @@ document.addEventListener("DOMContentLoaded", function() {
   canvas.addEventListener("mouseup", onEnd);
   canvas.addEventListener("touchend", onEnd);
   canvas.addEventListener("mouseout", onEnd);
+  
+  function drawPath() {
+    cntx.moveTo(...path[0]);
+    cntx.beginPath();
+    for (var point in path) {
+      cntx.lineTo(...path[point]);
+    }
+    cntx.stroke();
+  }
 
   function onStart(e) {
     isPainting = true;
-    path.push([e.offsetX,e.offsetY],[e.offsetX,e.offsetY]);
-    //two points are needed to make canvas's stroke show up
+    path.push([e.offsetX,e.offsetY],[e.offsetX,e.offsetY]); //two points are needed to make canvas's stroke show up for first point
     drawPath();
   }
 
@@ -124,78 +193,59 @@ document.addEventListener("DOMContentLoaded", function() {
   function onEnd() {
     if (isPainting === true) {
       isPainting = false;
+      if (isErasing === true) {
+        cntx.lineWidth = lineWidth.value;
+        cntx.strokeStyle = `rgb(255, 0, 0, ${intensity.value})`; //reset to regular painting
+        isErasing = false;
+      }
       path = [];
       imgdata.push(cntx.getImageData(0,0,canvas.width, canvas.height));
     }
   }
-
-  function drawPath() {
-    cntx.moveTo(...path[0]);
-    cntx.beginPath();
-    for (var point in path) {
-      cntx.lineTo(...path[point]);
-    }
-    cntx.stroke();
-  }
   //end of main drawing functions
 
   //drawing triggers functions
-  let triggerbutton = document.getElementById('trigger');
-  let triggertypebox = document.getElementById("triggertypes");
-
-  triggerbutton.addEventListener('click', (e) => {
-    canvas.removeEventListener("touchstart",onStart);
-    canvas.removeEventListener("mousedown",onStart);
-    canvas.addEventListener('mousedown', drawtrigger);
-    canvas.addEventListener('touchstart', drawtrigger);
-    canvas.addEventListener("mouseup", triggerstamp);
-    canvas.addEventListener("touchend", triggerstamp);
-
-    triggertypebox.style.display = "contents"; //to make it not hidden
-  });
-
   function drawtrigger(e) {
     cntx.lineWidth = 1;
     cntx.strokeStyle = `rgb(0, 0, 0, 1)`;
     path.push([e.offsetX,e.offsetY]);
   }
 
+  function vectorizePath(A,B,offset) {
+    let d = Math.sqrt(Math.pow(B[0]-A[0],2)+ Math.pow(B[1]-A[1],2));
+    let u = [(B[0]-A[0])/d, (B[1]-A[1])/d];
+    return [B[0]+offset*u[0],B[1]+offset*u[1]];
+  }
+
   function triggerstamp(e) {
     path.push([e.offsetX,e.offsetY]);
     let A = path[0];
     let B = path[1];
-    let x = 0;
-    let y = 1;
-    let d = Math.sqrt(Math.pow(B[x]-A[x],2)+ Math.pow(B[y]-A[y],2));
-    let u = [(B[x]-A[x])/d, (B[y]-A[y])/d];
     let offset = 20;
-    let v = [B[x]+offset*u[x],B[y]+offset*u[y]];
-
+    let v = vectorizePath(A,B,offset);
 
     cntx.lineTo(e.offsetX,e.offsetY);
     cntx.beginPath();
-    cntx.moveTo(A[x],A[y]);
-    cntx.lineTo(B[x],B[y]);
+    cntx.moveTo(A[0],A[1]);
+    cntx.lineTo(B[0],B[1]);
     cntx.stroke();
     let radius = 10;
-    cntx.moveTo(v[x]+radius,v[y]);
+    cntx.moveTo(v[0]+radius,v[1]);
     let triggerspacing = 0;
     for (let key in triggers){
-      if (triggers[key][key]=== 'custom') {
-        
+      if (triggers[key][key]=== 'custom' && triggers[key].element.checked) {        
+        cntx.font = "16px Arial";
+        cntx.fillText(document.getElementById('customtext').value, v[0]-13.5+triggerspacing,v[1]-13.5)
       } else {
         if(triggers[key].element.checked) {
-          // console.log('triggers[key].radiobetter', triggers[key].radiobetter);
-          // console.log('triggers[key]radiobetter.checked', triggers[key].radiobetter.checked);
 
-          cntx.drawImage(triggers[key].icon, v[x]-13.5+triggerspacing,v[y]-13.5,25,25);
+          cntx.drawImage(triggers[key].icon, v[0]-13.5+triggerspacing,v[1]-13.5,25,25);
           triggerspacing = triggerspacing + offset;
 
           if (triggers[key].radiobetter.checked) {
-            // console.log('radiobetterifcheck');
-            cntx.drawImage(makebetter, v[x]+triggerspacing-36,v[y]-12,40,40);
+            cntx.drawImage(makebetter, v[0]+triggerspacing-36,v[1]-12,40,40);
           } else if (triggers[key].radioworse.checked) {
-            cntx.drawImage(makesworse, v[x]+triggerspacing-36,v[y]-12,40,40);
+            cntx.drawImage(makesworse, v[0]+triggerspacing-36,v[1]-12,40,40);
           }
         } 
           
@@ -205,23 +255,9 @@ document.addEventListener("DOMContentLoaded", function() {
     cntx.stroke();
     path = [];
 
-    canvas.removeEventListener("mouseup", triggerstamp);
-    canvas.removeEventListener("touchend", triggerstamp);
-    canvas.removeEventListener('mousedown', drawtrigger);
-    canvas.removeEventListener('touchstart', drawtrigger);
-    canvas.addEventListener("mousedown",onStart);
-    canvas.addEventListener("touchstart",onStart);
-
     imgdata.push(cntx.getImageData(0,0,canvas.width, canvas.height));
-    cntx.lineWidth = lineWidth.value;
-    cntx.strokeStyle = `rgb(255, 0, 0, ${intensity.value})`;
 
-    triggertypebox.style.display = "none";
-    for (let key in triggers) {
-      triggers[key].radio.style.display = 'none';
-      triggers[key].element.checked = false;
-      
-    }
+    closetriggers();
   }
 
 })
